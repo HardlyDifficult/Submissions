5f244b661d91107ad7c9b7ffbb34ed85f8fa1889
Serafin Kaiser <serafinkaiser@yahoo.de>
2017-10-05 23:28:35 +0200

    Moving Tractor with examlpe Scene

---
+++ TheCoCe/TBS/Assets/Scripts/SplineWalker.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+
+public enum RearHydraulicsState
+{
+    ToTop,
+    ToMiddle,
+    ToBottom
+}
+
+public class SplineWalker : MonoBehaviour {
+    public BezierSpline spline;
+
+    public static int samples = 1000;
+
+    public float duration;
+    public bool checkBehind;
+    public float vehicleLength;
+
+    [Range(0f, 1f)]
+    public float steerAhead;
+    public Transform[] wheelsSteer;
+
+
+    //Defines what and when to play an animation
+    [System.Serializable]
+    public struct RearHydraulics
+    {
+        public RearHydraulicsState equipmentState;
+        public float progress;
+    }
+    public RearHydraulics[] rearAnimation;
+
+    //Defines the wheels and their radius to roll on the ground
+    [System.Serializable]
+    public struct Wheel
+    {
+        public Transform wheel;
+        public float radius;
+        public bool invertRotation;
+    }
+    public Wheel[] wheels;
+
+
+    private float progress;
+    public float Progress
+    {
+        get
+        {
+            return progress;
+        }
+    }
+
+    private Vector3 lastPosition;
+    private float splineLength;
+
+    private void Start()
+    {
+        splineLength = 0f;
+        Vector3 prev = spline.GetPoint(0);
+        for (int i = 0; i < samples; i++)
+        {
+            Vector3 point = spline.GetPoint((1f / (float)samples) * i);
+            splineLength += (prev - point).magnitude;
+            prev = point;
+
+        }
+    }
+
+    void Update () {
+        progress += Time.deltaTime / duration;
+        
+        //loop or don't loop
+        if (progress > 1f)
+        {
+            progress = spline.Loop ? 0f : 1f;
+        }
+        Vector3 position = spline.GetPoint(progress);
+        transform.localPosition = position;
+
+        //Rotate wheels depending on distance
+        float distance = Vector3.Distance(lastPosition, position);
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            if (wheels[i].wheel != null)
+            {
+                float wheelRot = distance / wheels[i].radius;
+                if (wheels[i].invertRotation)
+                    wheels[i].wheel.Rotate(new Vector3(0f, (wheelRot * 180 / Mathf.PI) * -1, 0f));
+                else
+                    wheels[i].wheel.Rotate(new Vector3(0f, wheelRot * 180 / Mathf.PI, 0f));
+
+            }
+        }
+        lastPosition = position;
+
+        //Align rear of the vehicle to the spline
+        if (checkBehind)
+        {
+            float percent = (vehicleLength / splineLength);
+            float abs = progress - percent;
+
+            if (abs < 0)
+                abs = 1 + abs;
+
+            transform.LookAt(spline.GetPoint(abs));
+        }
+
+        //Steer wheels
+        for (int i = 0; i < wheelsSteer.Length; i++)
+        {
+            if (wheelsSteer[i] != null)
+            {
+                Vector3 pos = wheelsSteer[i].position + spline.GetDirection((progress + steerAhead) % 1f);
+                float distanceToPlane = Vector3.Dot(wheelsSteer[i].up, pos - wheelsSteer[i].position);
+                Vector3 pointOnPlane = pos - (wheelsSteer[i].up * distanceToPlane);
+        
+                wheelsSteer[i].LookAt(pointOnPlane, wheelsSteer[i].up);
+            }
+        }
+	}
+}
--- TheCoCe/TBS/Assets/Scripts/Tractor.cs
+++ TheCoCe/TBS/Assets/Scripts/Tractor.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Tractor : MonoBehaviour {
 
     public Transform equipment;
+   
+    //I had issues getting these in code so I just assigned them in the inspector
+    public SplineWalker splineWalker;
+    public Animator animator;
 
-    //Can be set by hand as well just in case
+    private RearHydraulicsState state;
     private Transform referenceUpper; 
     private Transform referenceLower;
 
@@ -18,15 +22,58 @@ public class Tractor : MonoBehaviour {
             referenceLower = this.transform.Find("Tractor/Main/3Point01/3Point03/3Point02/3PointLowerRef");
 	}
 	
-	// Sets Position and rotation of the equipment
 	void Update () {
+        //Positioning Plough
         if (equipment != null && referenceLower != null && referenceUpper != null)
         {
             equipment.transform.position = referenceLower.position;
+
+            //This is causing issues with jittering (flipping from y = 0 to y = 180) 
+            //You can see this when moving referenceUpper back and forth in play mode
             equipment.LookAt(referenceUpper);
             equipment.transform.Rotate(new Vector3(1.0f, 0, 0), 90);
         }
 
+        //Plays animations set in the SplineWalker. This is kinda ugly though
+        if (splineWalker != null)
+        {
+            for (int i = 0; i < splineWalker.rearAnimation.Length; i++)
+            {
+                float dist = splineWalker.rearAnimation[i].progress - splineWalker.Progress;
+                if (dist < 0.01 && dist > -0.01)
+                {
+                    switch (splineWalker.rearAnimation[i].equipmentState)
+                    {
+                        case RearHydraulicsState.ToTop:
+                            if (state != RearHydraulicsState.ToTop)
+                            {
+                                animator.Play("Tractor|MidToHigh");
+                                state = RearHydraulicsState.ToTop;
+                            }
+                            break;
+                        case RearHydraulicsState.ToMiddle:
+                            if (state != RearHydraulicsState.ToMiddle)
+                            {
+                                if (state == RearHydraulicsState.ToTop)
+                                    animator.Play("Tractor|HighToMid");
+                                else if (state == RearHydraulicsState.ToBottom)
+                                    animator.Play("Tractor|LowToMid");
+                                state = RearHydraulicsState.ToMiddle;
+                            }
+                            break;
+                        case RearHydraulicsState.ToBottom:
+                            if (state != RearHydraulicsState.ToBottom)
+                            {
+                                animator.Play("Tractor|MidToLow");
+                                state = RearHydraulicsState.ToBottom;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
 	}
 
 }
